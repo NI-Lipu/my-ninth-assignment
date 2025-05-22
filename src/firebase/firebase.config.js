ca7eb878b2397aad2c9591a584f890837f762d0b
NI Lipu <nilipu500@gmail.com>
2025-05-22 20:03:02 +0600

    Add basic project set up like react-router-dom, tailwind css, daisyui,firebase and some file like route,mainlayout,firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+   apiKey: 'AIzaSyDa91v4alqXUOlOUykCj4OJmgr7rx37KQ4',
+   authDomain: 'my-ninth-assignment-c9b53.firebaseapp.com',
+   projectId: 'my-ninth-assignment-c9b53',
+   storageBucket: 'my-ninth-assignment-c9b53.firebasestorage.app',
+   messagingSenderId: '916152381358',
+   appId: '1:916152381358:web:55dc3435388e87cd54223f',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+export default app
---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,5 @@
+const MainLayout = () => {
+   return <div className="">MainLayout</div>
+}
+
+export default MainLayout
